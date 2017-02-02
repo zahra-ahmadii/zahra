6eba6c209d1a048d62d852fda4a240bc38fe2da6
zahra-ahmadii <nnedayi@ymail.com>
2017-02-02 05:23:20 -0800

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,8 +1,8 @@
-
+import math
 tokens = ('NUMBER','PLUS','MENHA','ZARB','TAGHSIM','PARANTEZBAZ','PRANTEZBASTE',
           'RESHTE','MOSAVI','INTMAIN','KOROSHEBAZ','KOROSHEBASTE','IF','ELSE'
           ,'WHILE','PRINT','INPUT','COTEITION','BODY','EQUAL','NOTEQUAL','K',
-          'BO','BM','KM','KAMA','SIN','COS','TAN','POW','SQRT') 
+          'BO','BM','KM','KAMA','SIN','COS','TAN','POW','SQRT','INT','DOUBLE','STRING','FASELE') 
 t_PLUS    = r'\+'
 t_MENHA   =r'\-'
 t_ZARB=r'\*'
@@ -13,7 +13,18 @@ t_KAMA=r','
 dic={}
 memID=0
 lbl_counter=0
-
+def t_INT(t):
+    r'int'
+    return t
+def t_DOUBLE(t):
+    r'double'
+    return t
+def t_STRING(t):
+    r'string'
+    return t
+def t_FASELE(t):
+    r'\ '
+    return t
 def t_PARANTEZBAZ(t):
     r'\('
     return t
@@ -127,9 +138,28 @@ def p_b(p):
 def p_la(p):
     'A : '
     #p[0]=p[1]
+def p_type(p):
+    'A : TYPE RESHTE'
+    dic[p[2]]=[p[1]]
+    print(p[2])
+def p_type1(p):
+    'TYPE : INT'
+    p[0]=p[1]
+    print(p[0])
+def p_type2(p):
+    'TYPE : DOUBLE'
+    p[0]=p[1]
+    print(p[0])
+def p_type3(p):
+    'TYPE : STRING'
+    p[0]=p[1]
+    print(p[0])
 def p_w(p):
     'A : WHILE PARANTEZBAZ CONDITION  PRANTEZBASTE KOROSHEBAZ A KOROSHEBASTE'
-    p[0]=["IF" ,p[3],' JUMP ','LABLE',lable_counter(),p[6],'\n','ELSE','JUMP','LABLE',lable_counter(),':\n']
+    L1='L'+lable_counter()
+    L2='L'+lable_counter()
+    p[0]=[L1,p[3],'jnz'+ L2,p[6],'goto'+L1,L2]
+   # p[0]=["WHILE" ,p[3],' JUMP ','LABLE',lable_counter(),p[6],'\n','ELSE','JUMP','LABLE',lable_counter(),':\n']
     print(p[0])
     
 '''def p_if(p):
@@ -140,15 +170,16 @@ def p_w(p):
     
 def p_ife(p):
     'A : IF PARANTEZBAZ CONDITION2 PRANTEZBASTE KOROSHEBAZ A  KOROSHEBASTE ELSE KOROSHEBAZ A KOROSHEBASTE '
-    p[0]=["IF" ,p[3],' JUMP ','LABLE',lable_counter(),'ELSE',p[6],'\n','LABLE',lable_counter(),'GOTO L2','L1:',p[10],'L2:'':\n']
+    L1='L'+lable_counter()
+    L2='L'+lable_counter()
+    p[0]=[p[3],'jnz'+L1,p[6],'goto'+ L2, L1 ,p[10],L2]
+    #p[0]=["IF" ,p[3],' JUMP ','LABLE',lable_counter(),'ELSE','\n','LABLE',lable_counter(),':\n']
     print(p[0])
     
 def p_input(p):
     r'A : RESHTE MOSAVI INPUT PARANTEZBAZ PRANTEZBASTE'
-    if p[1] not in dic:
-        
-        print(p[1] +" is undifined")
-
+    p[0]=p[1]
+    p[0]=input()
 def p_PRINT(p):
     'A : PRINT PARANTEZBAZ DD RESHTE DD PRANTEZBASTE'
     p[0]=('\''+p[4]+'\'')
@@ -159,12 +190,13 @@ def p_RESHTE2(p):
    p[0]=p[1]
 def p_RESHTE1(p):
    'E : RESHTE MOSAVI E'
-   p[1]=p[3]
-   p[0]=p[1]
    dic[p[1]]=p[3]
+   #p[1]=p[3]
+   p[0]=p[1]
+   
    global x
    x=p[1]
-   print('کاهش با قانون s->RESHTE=E',p[1])
+   print(p[1],'=',dic[p[1]])
 def p_CONDITION2(p):
     'CONDITION2 : TY K TY'
     if(p[2]=='<'):
@@ -180,25 +212,36 @@ def p_ty2(p):
     'TY : F'
     p[0]=p[1]
 def p_sin(p):
-    'A : RESHTE MOSAVI SIN PARANTEZBAZ E PRANTEZBASTE'
-    p[1]=sin(p[5])
+    'A : RESHTE MOSAVI SIN PARANTEZBAZ NUMBER PRANTEZBASTE'
+    
+    #p[1]=math.sin(float(p[5]))
+   # p[0]=p[1]
+    dic[p[1]]=[math.sin(float(p[5]))]
     p[0]=p[1]
+    print(p[1],'=',dic[p[1]])
+   
 def p_cos(p):
     'A : RESHTE MOSAVI COS PARANTEZBAZ E PRANTEZBASTE'
-    p[1]=cos(p[5])
+    dic[p[1]]=[math.cos(float(p[5]))]
     p[0]=p[1]
+    print(p[1],'=',dic[p[1]])
+   
 def p_tan(p):
     'A : RESHTE MOSAVI TAN PARANTEZBAZ E PRANTEZBASTE'
-    p[1]=tan(p[5])
+    dic[p[1]]=[math.tan(float(p[5]))]
     p[0]=p[1]
+    print(p[1],'=',dic[p[1]])
+   
 def p_pow(p):
     'A : RESHTE MOSAVI POW PARANTEZBAZ E KAMA E PRANTEZBASTE'
-    p[1]=pow(p[5],p[7])
+    dic[p[1]]=[math.pow(float(p[5]),float(p[7]))]
     p[0]=p[1]
+    print(p[1],'=',dic[p[1]])
 def p_sqrt(p):
     'A : RESHTE MOSAVI SQRT PARANTEZBAZ E PRANTEZBASTE'
-    p[1]=sqrt(p[5])
+    dic[p[1]]=[math.sqrt(float(p[5]))]
     p[0]=p[1]
+    print(p[1],'=',dic[p[1]])
 def p_DD(p):
     r'DD : COTEITION'
     p[0]=p[1]
@@ -210,26 +253,33 @@ def p_DD2(p):
     P[0]=P[1]'''
 def p_e_e_t(p):
    'E : E PLUS T'
-   #p[0] = p[1]+p[3]
+   
    oprand1 =''
    oprand2 =''
    beforCode = ''
    if type(p[1]) == tuple:
         oprand1 = p[1][1]
+        #a=float(oprand1)
         beforCode += p[1][0]
    else:
         oprand1 = p[1]
+        #a=float(oprand1)
    if type(p[3]) == tuple:
         oprand2 = p[3][1]
+        #b=float(oprand2)
         beforCode += p[3][0]
    else:  
         oprand2 = p[3]
-
+        #b=float(oprand2)
+   
    mem = memAloc()
    p[0] = (beforCode + 'ADD ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
-  # print(p[0])
-   global x
-   x=p[0]
+   #c = a+b
+   #print(c)
+   #a=oprand1
+   #b=oprand2
+   #c=a+b
+   
    #print('کاهش با قانون E → E + E  ',p[0])
 def p_e_e_t2(p):
     'E : E MENHA T' 
@@ -250,6 +300,7 @@ def p_e_e_t2(p):
 
     mem = memAloc()
     p[0] = (beforCode + 'SUB ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
+    oprand3=oprand1-oprand2
     global x
     x=p[0]
     #print('کاهش با قانون E → E - E  ',p[0])
@@ -261,23 +312,32 @@ def p_e_t(p):
    #print('کاهش با قانون E->T ',p[0])
 def p_t_t_f(p):
     'T : T ZARB F' 
-    #p[0] = p[1]*p[3]
+   
     oprand1 =''
     oprand2 =''
     beforCode = ''
     if type(p[1]) == tuple:
         oprand1 = p[1][1]
+        #a=float(oprand1)
         beforCode += p[1][0]
     else:
         oprand1 = p[1]
+        #a=float(oprand1)
     if type(p[3]) == tuple:
         oprand2 = p[3][1]
+       # b=float(oprand2)
         beforCode += p[3][0]
     else:  
         oprand2 = p[3]
-
+        #b=float(oprand2)
+    
     mem = memAloc()
     p[0] = (beforCode + 'MUL ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
+    #c=a*b
+    #print(c)
+    #a=oprand1
+    #c=a*b
+    
     global x
     x=p[0]
     #print('کاهش با قانون T → T * F  ',p[0])
@@ -299,7 +359,8 @@ def p_t_t_f2(p):
         oprand2 = p[3]
 
     mem = memAloc()
-    p[0] = (beforCode + 'ADD ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
+    p[0] = (beforCode + 'TAGHSIM ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
+    oprand3=oprand1/oprand2
     global x
     x=p[0]
    # print('کاهش با قانون T → T / F  ',p[0])
@@ -361,12 +422,33 @@ f2_read=open("zari.txt","r")
 while True:
   
    line1=f2_read.readline() 
-   if line1=="END" :
+   if line1=="END\n" :
        f2_read.close()
+       print(222)
        break
    else:
     p.parse(line1)
     
     
+import re
+f_read1=open("zari_code.txt","r")
+araye={}
+while(1):
+ line2=f_read1.readline()
+ order=line2
+ result=re.match("END",order)
+ if(result):
+     break
+resulu=re.match("ADD (\[[0-9]+(\.[0-9]+)?/]),(/[[0-9]+(\.[0-9]+)?/]),(/[\d+/])",order)
+if(result):
+    a=araye[result.group(1)]
+    b=araye[result.group(2)]
+    araye[result.group(3)]=a+b
+resulu=re.match("MUL (\[[0-9]+(\.[0-9]+)?/]),(/[[0-9]+(\.[0-9]+)?/]),(/[\d+/])",order)
+if(result):
+    a=araye[result.group(1)]
+    b=araye[result.group(2)]
+    araye[result.group(3)]=a*b
+print(araye)
    
    
