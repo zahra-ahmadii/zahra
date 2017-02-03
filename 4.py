93cca037bd7ff3889660a3b28c6a5125184ee88c
zahra-ahmadii <nnedayi@ymail.com>
2017-02-03 11:45:36 -0800

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,4 +1,5 @@
 import math
+import re
 tokens = ('NUMBER','PLUS','MENHA','ZARB','TAGHSIM','PARANTEZBAZ','PRANTEZBASTE',
           'RESHTE','MOSAVI','INTMAIN','KOROSHEBAZ','KOROSHEBASTE','IF','ELSE'
           ,'WHILE','PRINT','INPUT','COTEITION','BODY','EQUAL','NOTEQUAL','K',
@@ -13,6 +14,8 @@ t_KAMA=r','
 dic={}
 memID=0
 lbl_counter=0
+f3=open("zari3.txt","w")
+global flag,y
 def t_INT(t):
     r'int'
     return t
@@ -140,8 +143,8 @@ def p_la(p):
     #p[0]=p[1]
 def p_type(p):
     'A : TYPE RESHTE'
-    dic[p[2]]=[p[1]]
-    print(p[2])
+    dic[p[2]]=[p[1],memAloc()]
+    print(dic[p[2]])
 def p_type1(p):
     'TYPE : INT'
     p[0]=p[1]
@@ -180,23 +183,31 @@ def p_input(p):
     r'A : RESHTE MOSAVI INPUT PARANTEZBAZ PRANTEZBASTE'
     p[0]=p[1]
     p[0]=input()
+    #dic.update(dic[p[1]])=p[0]
+    #dic[p[1]].append(p[0])
 def p_PRINT(p):
     'A : PRINT PARANTEZBAZ DD RESHTE DD PRANTEZBASTE'
     p[0]=('\''+p[4]+'\'')
     print(p[0])
 def p_RESHTE2(p):
-   'E : RESHTE MOSAVI RESHTE'
+   'A : RESHTE MOSAVI RESHTE'
    p[1]=p[3]
    p[0]=p[1]
 def p_RESHTE1(p):
-   'E : RESHTE MOSAVI E'
-   dic[p[1]]=p[3]
+   'A : RESHTE MOSAVI E'
+   if type(p[3]) == tuple:
+        print(888)
+        print(p[3][0])
+        #print(dic.get(p[1])
+        print(dic[p[1]][1],'=',str(p[3][1]),'\n')
+        f3.writelines(p[3][0])
+        f3.writelines(dic[p[1]][1]+"="+str(p[3][1])+'\n')
+   else:
+    dic[p[1]]=p[3]
    #p[1]=p[3]
-   p[0]=p[1]
-   
-   global x
-   x=p[1]
-   print(p[1],'=',dic[p[1]])
+    p[0]=p[1]
+   #print(p[1][0])
+    print(p[1],'=',dic[p[1]])
 def p_CONDITION2(p):
     'CONDITION2 : TY K TY'
     if(p[2]=='<'):
@@ -300,7 +311,7 @@ def p_e_e_t2(p):
 
     mem = memAloc()
     p[0] = (beforCode + 'SUB ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
-    oprand3=oprand1-oprand2
+    #oprand3=oprand1-oprand2
     global x
     x=p[0]
     #print('کاهش با قانون E → E - E  ',p[0])
@@ -360,7 +371,7 @@ def p_t_t_f2(p):
 
     mem = memAloc()
     p[0] = (beforCode + 'TAGHSIM ' + oprand1 + ',' + oprand2 + ','+ mem+'\n' ,mem)
-    oprand3=oprand1/oprand2
+    #oprand3=oprand1/oprand2
     global x
     x=p[0]
    # print('کاهش با قانون T → T / F  ',p[0])
@@ -428,27 +439,86 @@ while True:
        break
    else:
     p.parse(line1)
-    
-    
+f3.writelines('END\n')  
+f3.close()    
 import re
-f_read1=open("zari_code.txt","r")
+f_read1=open("zari3.txt","r")
 araye={}
 while(1):
  line2=f_read1.readline()
  order=line2
- result=re.match("END",order)
+ result=re.match("END\n",order)
  if(result):
      break
-resulu=re.match("ADD (\[[0-9]+(\.[0-9]+)?/]),(/[[0-9]+(\.[0-9]+)?/]),(/[\d+/])",order)
-if(result):
-    a=araye[result.group(1)]
-    b=araye[result.group(2)]
-    araye[result.group(3)]=a+b
-resulu=re.match("MUL (\[[0-9]+(\.[0-9]+)?/]),(/[[0-9]+(\.[0-9]+)?/]),(/[\d+/])",order)
-if(result):
-    a=araye[result.group(1)]
-    b=araye[result.group(2)]
-    araye[result.group(3)]=a*b
+ result=re.match(r'ADD (\[?)((\d+)|(\d+\.\d+))(\]?),(\[?)((\d+)|(\d+\.\d+))(\]?),\[(\d+)\]',order)
+ #print(result)
+ if(result):
+    if(result.group(1)==''):
+        a=float(result.group(2))
+    else:
+        a=araye[result.group(2)]
+   # print(a)
+    if(result.group(6)==''):
+      b=float(result.group(7))
+    else:
+        b=araye[result.group(7)]
+    #print(b)
+    
+    #y1=result.group(11)
+    araye[result.group(11)]=a+b
+ result=re.match(r'SUB (\[?)((\d+)|(\d+\.\d+))(\]?),(\[?)((\d+)|(\d+\.\d+))(\]?),\[(\d+)\]',order)
+ #print(result)
+ if(result):
+    if(result.group(1)==''):
+        a=float(result.group(2))
+    else:
+        a=araye[result.group(2)]
+   # print(a)
+    if(result.group(6)==''):
+      b=float(result.group(7))
+    else:
+        b=araye[result.group(7)]
+    #print(b)
+    
+    #y1=result.group(11)
+    araye[result.group(11)]=a-b
+ result=re.match(r'MUL (\[?)((\d+)|(\d+\.\d+))(\]?),(\[?)((\d+)|(\d+\.\d+))(\]?),\[(\d+)\]',order)
+ if(result):
+    #print(555)
+    if(result.group(1)==''):
+      a=int(result.group(2))
+    else:
+        a=araye[result.group(2)]
+    #print(a)
+    if(result.group(6)==''):
+      b=int(result.group(7))
+    else:
+        b=araye[result.group(7)]
+    
+    #y1=result.group(11)
+    araye[result.group(11)]=a*b
+ result=re.match(r'TAGHSIM (\[?)((\d+)|(\d+\.\d+))(\]?),(\[?)((\d+)|(\d+\.\d+))(\]?),\[(\d+)\]',order)
+ #print(result)
+ if(result):
+    if(result.group(1)==''):
+        a=float(result.group(2))
+    else:
+        a=araye[result.group(2)]
+   # print(a)
+    if(result.group(6)==''):
+      b=float(result.group(7))
+    else:
+        b=araye[result.group(7)]
+    #print(b)
+    
+    #y1=result.group(11)
+    araye[result.group(11)]=a/b
+ result=re.match(r'\[(\d+)\] = \[(\d+)\]\n',order)
+ if(result):
+        print(557)
+        print(result.group(1))
+        araye[result.group(1)]=araye[result.group(2)]
+
 print(araye)
-   
-   
+
+#print(dic)   
